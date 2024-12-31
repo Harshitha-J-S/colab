96c3042440cfa3983bafe158a91ea8746aacb72a
Haksham <harsh924hashvm@gmail.com>
2024-12-31 19:09:57 +0530

    new changes

--- popup.js
+++ popup.js
@@ -1,28 +1,35 @@
-// function click() {
-//     let txt = document.getElementById("txt");
-//     if (txt) {
-//         if (txt.innerHTML === "champu") {
-//             txt.innerHTML = "Original Text";
-//         } else {
-//             txt.innerHTML = "champu";
-//         }
-//     } else {
-//         console.error('Element with id "txt" not found.');
-//     }
-// }
+console.log('This is a popup!');
+
+function click() {
+    let txt = document.getElementById("txt");
+    if (txt) {
+        if (txt.innerHTML === "champu") {
+            txt.innerHTML = "Original Text";
+        } else {
+            txt.innerHTML = "champu";
+        }
+    } else {
+        console.error('Element with id "txt" not found.');
+    }
+}
 
 document.getElementById("changeTextButton").addEventListener("click", click);
 
 document.getElementById('readButton').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: () => {
-          const element = document.querySelector('.hP');
-          return element ? element.textContent : 'Element with class "hP" not found';
-        }
-      }, (results) => {
-        document.getElementById('htmlContent').value = results[0].result;
-      });
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: () => {
+                const hPElement = document.querySelector('.hP');
+                const goElement = document.querySelector('.go');
+                const bod = document.querySelector('.a3s');
+                const hPText = hPElement ? hPElement.textContent : 'Element with class "hP" not found';
+                const goText = goElement ? goElement.textContent : 'Element with class "go" not found';
+                const bodText = bod ? bod.textContent : 'Element with class "a3s" not found';
+                return `${hPText}\n${goText}\n${bodText}`;
+            }
+        }, (results) => {
+            document.getElementById('htmlContent').value = results[0].result;
+        });
     });
-  });
+});
